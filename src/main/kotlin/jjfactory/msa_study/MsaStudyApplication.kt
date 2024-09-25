b4921213ba8b9b2e9656292d39b214f19cfb16e9
jaehyeong <wogud1514@gmail.com>
2024-09-25 15:17:10 +0900

    init

---
+++ src/main/kotlin/jjfactory/msa_study/MsaStudyApplication.kt
@@ -0,0 +1,11 @@
+package jjfactory.msa_study
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MsaStudyApplication
+
+fun main(args: Array<String>) {
+	runApplication<MsaStudyApplication>(*args)
+}
